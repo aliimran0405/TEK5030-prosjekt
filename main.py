3223962edd95a80cf45d0180a846608cf8f3b08e
Vetle Olavesen <vetleolavesen@gmail.com>
2025-05-08 14:36:47 +0200

    Update main to include segmentation model and videowriter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cv2
 import os
+from ultralytics import solutions, YOLO
+import torch
+import time
+
 
 # Path for where you store your input vids
 INPUT_VID_DIR_PATH = "./input_vid/" 
@@ -15,11 +19,20 @@ vid_idx = int(input("Which video would you like to process? (Enter idx) \n").str
 
 # Init capture object for video file
 cap = cv2.VideoCapture(INPUT_VID_DIR_PATH + inp_vids[0])
-
-
 if (not cap.isOpened()):
     raise Exception("Could not find video stream")
 
+
+# Video writer
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+video_writer = cv2.VideoWriter("instance_segmentation_result.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps=fps, frameSize=(w,h))
+
+isegment = solutions.InstanceSegmentation(show=False,
+                                          model="yolo11n-seg.pt",
+                                          tracker="botsort.yaml",
+                                          classes=[0],
+                                          device='cuda' if torch.cuda.is_available() else 'cpu'
+                                          )
 
 
 # Read video file frame for frame until finished
@@ -27,7 +40,9 @@ while (cap.isOpened()):
     ret, frame = cap.read()
 
     if ret == True:
-        cv2.imshow('Frame', frame)
+        #cv2.imshow('Frame', frame)
+        results = isegment(frame)
+        video_writer.write(results.plot_im)
 
         # Press q to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -41,4 +56,5 @@ while (cap.isOpened()):
 
 # When done reading video file: release capture object and close all cv2 windows.
 cap.release()
+video_writer.release()
 cv2.destroyAllWindows()
